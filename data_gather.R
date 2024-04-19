72e765a6511adf4d4fe01952c35bb790849201b9
gamex2 <krmhominho@live.com>
2024-04-19 09:57:49 +0000

    fixed cannibal error

--- data_gather.R
+++ data_gather.R
@@ -171,7 +171,7 @@ Stomach_fish_candat <- Stomach_fish_candat %>%
 
 Stomach_fish_candat <- merge(Stomach_fish_candat, fry_abundance[prey_sp == "candat"], by = "Year", all.x = T)
 set.seed(3333)
-summary(glm(data = Stomach_fish_candat, formula = cannibal ~ size_class+Year+Fry_abundance, family = binomial(link = "logit")))
+summary(glm(data = Stomach_fish_candat, formula = cannibal ~ Year+size_class+Fry_abundance, family = binomial(link = "logit")))
 
 Stomach_fish_candat2 <- Stomach_fish_candat
 Stomach_fish_candat2$cannibal[Stomach_fish_candat2$cannibal == 0] <- "no"
@@ -235,21 +235,26 @@ ggpubr::ggarrange(g.yoy, g.old, g.older,
                   ncol = 2, nrow = 2,
                   common.legend = TRUE, legend = "bottom")
 
-Stomach_fish_candat_melt <- setDT(melt(Stomach_fish_candat[, .(ct_catchid, SL, Year, Species, size_class, cannibal, candat, ouklej, 
+Stomach_fish_candat_melt <- setDT(melt(Stomach_fish_candat[, .(ct_catchid, SL, Year, Species, size_class, candat, ouklej, 
                                                                okoun, plotice, jezdik, cejn, cejnek, kaprovitka, okounovitá, Unknown)], 
-                                          id.vars = c("ct_catchid", "SL", "Year", "Species", "size_class", "cannibal"), 
+                                          id.vars = c("ct_catchid", "SL", "Year", "Species", "size_class"), 
                                        variable.name = "prey", value.name = "prey_n"))
 Stomach_fish_candat_melt <- Stomach_fish_candat_melt[!prey_n==0,]
-summary(glm(data = Stomach_fish_candat_melt, formula = cannibal ~ size_class+Year+prey_n, family = binomial(link = "logit")))
+Stomach_fish_candat_melt <- Stomach_fish_candat_melt %>% 
+  mutate(cannibal = case_when((prey == "candat" & prey_n >= 1) ~ 1, #condition 1
+                              TRUE ~ 0)) #all other cases
+Stomach_fish_candat_melt <- merge(Stomach_fish_candat_melt, fry_abundance[prey_sp == "candat"], by = "Year", all.x = T)
+set.seed(3333)
+summary(glm(data = Stomach_fish_candat_melt, formula = cannibal ~ size_class+Year+prey_n+Fry_abundance, family = binomial(link = "logit")))
 
 Stomach_fish_candat_melt2 <- Stomach_fish_candat_melt
 Stomach_fish_candat_melt2$cannibal[Stomach_fish_candat_melt2$cannibal == 0] <- "no"
 Stomach_fish_candat_melt2$cannibal[Stomach_fish_candat_melt2$cannibal == 1] <- "yes"
 
-ggplot(Stomach_fish_candat_melt2, aes(x = as.factor(Year), y = prey_n)) +
+ggplot(Stomach_fish_candat_melt2, aes(x = as.factor(Year), y = prey_n, fill = forcats::fct_rev(cannibal))) +
   geom_col(position = "stack") +
   facet_wrap(~size_class, scales = "free") + 
-  labs(x="Year", y="fish n", fill="Cannibalism")+
+  labs(x="Year", y="Prey n", fill="Cannibalism")+
   theme(plot.title = element_text(size = 32, face = "bold"),
         axis.text.x = element_text(size = 28,angle = 45, hjust = 1.05, vjust = 1.05),
         axis.text.y = element_text(size = 28),
@@ -261,18 +266,77 @@ ggplot(Stomach_fish_candat_melt2, aes(x = as.factor(Year), y = prey_n)) +
   theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(),
         panel.background = element_blank(), axis.line = element_line(colour = "black"))
 
-Stomach_fish_candat_melt2 <- merge(Stomach_fish_candat_melt2, fry_abundance[prey_sp == "candat"], by = "Year", all.x = T)
+sc.yoy <- ggplot(Stomach_fish_candat_melt2[size_class == "YOY"], aes(x = as.factor(Year), y = prey_n, fill = forcats::fct_rev(cannibal))) +
+  geom_col(position = "stack") +
+  geom_line(data=Stomach_fish_candat_melt2[size_class == "YOY"], group=1, mapping=aes(x=as.factor(Year),y=Fry_abundance/15), linewidth = 3)+
+  scale_y_continuous(name = "Prey n", 
+                     sec.axis = sec_axis(~.*15, name = "Fry n per hectare"))+
+  labs(x="Year", fill = "cannibalism", linewidth = NULL, title="YOY")+
+  theme(plot.title = element_text(size = 32, face = "bold"),
+        axis.text.x = element_text(size = 28,angle = 45, hjust = 1.05, vjust = 1.05),
+        axis.text.y = element_text(size = 28),
+        strip.text = element_text(size = 26),
+        axis.title.x = element_text(size = 20),
+        axis.title.y = element_text(size = 20),
+        legend.title = element_text(size=28),
+        legend.text = element_text(size = 26, face = "italic"))+
+  theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(), axis.title.y.right = element_text(vjust=2),
+        panel.background = element_blank(), axis.line = element_line(colour = "black"),legend.position="bottom")
+
+sc.old <- ggplot(Stomach_fish_candat_melt2[size_class == "older<300"], aes(x = as.factor(Year), y = prey_n, fill = forcats::fct_rev(cannibal))) +
+  geom_col(position = "stack") +
+  geom_line(data=Stomach_fish_candat_melt2[size_class == "older<300"], group=1, mapping=aes(x=as.factor(Year),y=Fry_abundance/23), linewidth = 3)+
+  scale_y_continuous(name = "Prey n", 
+                     sec.axis = sec_axis(~.*23, name = "Fry n per hectare"))+
+  labs(x="Year", fill = "cannibalism", linewidth = NULL, title="older<300")+
+  theme(plot.title = element_text(size = 32, face = "bold"),
+        axis.text.x = element_text(size = 28,angle = 45, hjust = 1.05, vjust = 1.05),
+        axis.text.y = element_text(size = 28),
+        strip.text = element_text(size = 26),
+        axis.title.x = element_text(size = 20),
+        axis.title.y = element_text(size = 20),
+        legend.title = element_text(size=28),
+        legend.text = element_text(size = 26, face = "italic"))+
+  theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(), axis.title.y.right = element_text(vjust=2),
+        panel.background = element_blank(), axis.line = element_line(colour = "black"),legend.position="bottom")
+
+sc.older <- ggplot(Stomach_fish_candat_melt2[size_class == "older>300"], aes(x = as.factor(Year), y = prey_n, fill = forcats::fct_rev(cannibal))) +
+  geom_col(position = "stack") +
+  geom_line(data=Stomach_fish_candat_melt2[size_class == "older>300"], group=1, mapping=aes(x=as.factor(Year),y=Fry_abundance/9), linewidth = 3)+
+  scale_y_continuous(name = "Prey n", 
+                     sec.axis = sec_axis(~.*9, name = "Fry n per hectare"))+
+  labs(x="Year", fill = "cannibalism", linewidth = NULL, title="older>300")+
+  theme(plot.title = element_text(size = 32, face = "bold"),
+        axis.text.x = element_text(size = 28,angle = 45, hjust = 1.05, vjust = 1.05),
+        axis.text.y = element_text(size = 28),
+        strip.text = element_text(size = 26),
+        axis.title.x = element_text(size = 20),
+        axis.title.y = element_text(size = 20),
+        legend.title = element_text(size=28),
+        legend.text = element_text(size = 26, face = "italic"))+
+  theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(), axis.title.y.right = element_text(vjust=2),
+        panel.background = element_blank(), axis.line = element_line(colour = "black"),legend.position="bottom")
+
+ggpubr::ggarrange(sc.yoy, sc.old, sc.older,
+                  ncol = 2, nrow = 2,
+                  common.legend = TRUE, legend = "bottom")
+
+
 
 #fish size in the stomach
 Stomach_fish_candat_size <- setDT(melt(Stomach_fish_candat[, .(ct_catchid, SL, Year, Species, size_class, candat_1,candat_2,candat_3,candat_4, candat_5,
                                                               candat_6, candat_7, candat_8, candat_9, ouklej_1, ouklej_2, ouklej_3, okoun_1,
                                                               okoun_2, okoun_3, okoun_4, plotice_1, plotice_2, jezdik_1, jezdik_2, jezdik_3,
                                                               cejn_1, cejnek_1, cejnek_2, kaprovitka_1, kaprovitka_2,Unknown_1, Unknown_2,
-                                                              Unknown_3, Unknown_4, cannibal)], 
-                                     id.vars = c("ct_catchid", "Year", "Species", "SL", "size_class", "cannibal"), variable.name = "prey_sp", value.name = "prey_size"))
+                                                              Unknown_3, Unknown_4)], 
+                                     id.vars = c("ct_catchid", "Year", "Species", "SL", "size_class"), variable.name = "prey_sp", value.name = "prey_size"))
 Stomach_fish_candat_size <- Stomach_fish_candat_size[!prey_size == 0]
 Stomach_fish_candat_size[, ':='(ratio_prey = prey_size/SL)]
 Stomach_fish_candat_size$prey_sp <- sub("\\_.*", "", as.character(Stomach_fish_candat_size$prey_sp))
+Stomach_fish_candat_size <- Stomach_fish_candat_size %>% 
+  mutate(cannibal = case_when(prey_sp == "candat" ~ 1, #condition 1
+                              TRUE ~ 0)) #all other cases
+set.seed(3333)
 summary(glm(data = Stomach_fish_candat_size, formula = cannibal ~ size_class+Year+ratio_prey, family = binomial(link = "logit")))
 
 ggplot(Stomach_fish_candat_size, aes(as.factor(Year), ratio_prey)) +
@@ -310,7 +374,7 @@ ggplot(Stomach_fish_candat_size, aes(as.factor(Year), ratio_prey)) +
         panel.background = element_blank(), axis.line = element_line(colour = "black"))
 
 
-#okoun
+#okoun####
 Stomach_content_okoun <- Stomach_content_all[Species == "okoun"]
 size_lip_ok <- all_catch_lip[sp_speciesid == "okoun" & ct_sl > 1,.(Max = max(ct_sl),
                                                                      Min = min(ct_sl)),
@@ -347,12 +411,14 @@ Stomach_fish_okoun <- Stomach_content_okoun[Fish==1, ]
 Stomach_fish_okoun <- Stomach_fish_okoun %>%
   mutate(cannibal = case_when(okoun != 0 ~ 1, #condition 1
                               TRUE ~ 0)) #all other cases
+# fry_abundance <- setDT(readxl::read_xlsx(here::here('Stomach.content.all.xlsx'), sheet = "fry"))
+Stomach_fish_okoun <- merge(Stomach_fish_okoun, fry_abundance[prey_sp == "okoun"], by = "Year", all.x = T)
 
+summary(glm(data = Stomach_fish_okoun, formula = cannibal ~ Year+size_class+Fry_abundance, family = binomial(link = "logit")))
 Stomach_fish_okoun2 <- Stomach_fish_okoun
 Stomach_fish_okoun2$cannibal[Stomach_fish_okoun2$cannibal == 0] <- "no"
 Stomach_fish_okoun2$cannibal[Stomach_fish_okoun2$cannibal == 1] <- "yes"
-# fry_abundance <- setDT(readxl::read_xlsx(here::here('Stomach.content.all.xlsx'), sheet = "fry"))
-Stomach_fish_okoun2 <- merge(Stomach_fish_okoun2, fry_abundance[prey_sp == "okoun"], by = "Year", all.x = T)
+
 
 
 c.old <- ggplot(Stomach_fish_okoun2[size_class == "older<200"], aes(x = as.factor(Year), y = Fish, fill = forcats::fct_rev(cannibal))) +
@@ -393,19 +459,24 @@ ggpubr::ggarrange(c.old, c.older,
                   ncol = 2, nrow = 1,
                   common.legend = TRUE, legend = "bottom")
 
-Stomach_fish_okoun_melt <- setDT(melt(Stomach_fish_okoun[, .(ct_catchid, SL, Year, Species, size_class, cannibal, candat, ouklej,
+Stomach_fish_okoun_melt <- setDT(melt(Stomach_fish_okoun[, .(ct_catchid, SL, Year, Species, size_class, candat, ouklej,
                                                                okoun, plotice, jezdik, cejn, cejnek, kaprovitka, okounovitá, Unknown)],
-                                       id.vars = c("ct_catchid", "SL", "Year", "Species", "size_class", "cannibal"),
+                                       id.vars = c("ct_catchid", "SL", "Year", "Species", "size_class"),
                                        variable.name = "prey", value.name = "prey_n"))
 Stomach_fish_okoun_melt <- Stomach_fish_okoun_melt[!prey_n==0,]
+Stomach_fish_okoun_melt <- Stomach_fish_okoun_melt %>% 
+  mutate(cannibal = case_when(prey == "okoun" ~ 1, #condition 1
+                              TRUE ~ 0)) #all other cases
+summary(glm(data = Stomach_fish_okoun_melt, formula = cannibal ~ Year+size_class+prey_n, family = binomial(link = "logit")))
+
 Stomach_fish_okoun_melt2 <- Stomach_fish_okoun_melt
 Stomach_fish_okoun_melt2$cannibal[Stomach_fish_okoun_melt2$cannibal == 0] <- "no"
 Stomach_fish_okoun_melt2$cannibal[Stomach_fish_okoun_melt2$cannibal == 1] <- "yes"
 
-ggplot(Stomach_fish_okoun_melt, aes(x = as.factor(Year), y = prey_n, fill = prey)) +
+ggplot(Stomach_fish_okoun_melt2, aes(x = as.factor(Year), y = prey_n, fill = forcats::fct_rev(cannibal))) +
   geom_col(position = "stack") +
   facet_wrap(~size_class, scales = "free") +
-  labs(x="Year", y="fish n", fill="Prey sp")+
+  labs(x="Year", y="Prey n", fill="Cannibalism")+
   theme(plot.title = element_text(size = 32, face = "bold"),
         axis.text.x = element_text(size = 28,angle = 45, hjust = 1.05, vjust = 1.05),
         axis.text.y = element_text(size = 28),
@@ -422,11 +493,14 @@ Stomach_fish_okoun_size <- setDT(melt(Stomach_fish_okoun[, .(ct_catchid, SL, Yea
                                                                candat_6, candat_7, candat_8, candat_9, ouklej_1, ouklej_2, ouklej_3, okoun_1,
                                                                okoun_2, okoun_3, okoun_4, plotice_1, plotice_2, jezdik_1, jezdik_2, jezdik_3,
                                                                cejn_1, cejnek_1, cejnek_2, kaprovitka_1, kaprovitka_2,Unknown_1, Unknown_2,
-                                                               Unknown_3, Unknown_4, cannibal)],
-                                       id.vars = c("ct_catchid", "Year", "Species", "SL", "size_class", "cannibal"), variable.name = "prey_sp", value.name = "prey_size"))
+                                                               Unknown_3, Unknown_4)],
+                                       id.vars = c("ct_catchid", "Year", "Species", "SL", "size_class"), variable.name = "prey_sp", value.name = "prey_size"))
 Stomach_fish_okoun_size <- Stomach_fish_okoun_size[!prey_size == 0]
 Stomach_fish_okoun_size[, ':='(ratio_prey = prey_size/SL)]
 Stomach_fish_okoun_size$prey_sp <- sub("\\_.*", "", as.character(Stomach_fish_okoun_size$prey_sp))
+Stomach_fish_okoun_size <- Stomach_fish_okoun_size %>% 
+  mutate(cannibal = case_when(prey_sp == "okoun" ~ 1, #condition 1
+                              TRUE ~ 0)) #all other cases
 summary(glm(data = Stomach_fish_okoun_size, formula = cannibal ~ size_class+Year+ratio_prey, family = binomial(link = "logit")))
 
 
